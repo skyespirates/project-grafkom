5c339f9ac66d01f63d5fca8e9dca10b2a51f15ba
Skyes Crawford <akashiseijurou414@gmail.com>
2020-12-13 18:45:37 +0700

    add Assets

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D myBody;
+
+    public float moveSpeed = 2f;
+    
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        }
+    }
+
+    public void PlatformMove(float x)
+    {
+        myBody.velocity = new Vector2(x, myBody.velocity.y);
+    }
+
+
+}
